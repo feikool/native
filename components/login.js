213a44cf31a3153eea766c7338ca736e3dc35f0e
feikool <feikool@sina.com>
2017-05-12 13:56:43 +0800

    init

--- components/login.js
+++ components/login.js
@@ -3,15 +3,31 @@ import {
     StyleSheet,
     Text,
     View,
-    TouchableOpacity
+    TouchableOpacity,
+    Alert
 } from 'react-native';
 
 export default class Logino extends Component {
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    constructor(props){
+        super(props);
+    }
+    renderPress(str){
+        Alert.alert(str);
+        console.log(str)
+    }
     render() {
         return (
             <View>
                 <TouchableOpacity activeOpacity={.5}>
-                <Text>可以点击的文本</Text>
+                <Text
+                    onPress={()=>this.renderPress('鼠标点击')}
+                    onLongPress={()=>this.renderPress('鼠标长按')}>可以点击的文本</Text>
                 </TouchableOpacity>
             </View>
         );
