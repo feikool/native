d99694c8023ac48cc0e57e0516db71925b661045
feikool <feikool@sina.com>
2017-05-15 21:20:20 +0800

    init

--- components/login.js
+++ components/login.js
@@ -9,38 +9,63 @@ import {
     Alert
 } from 'react-native';
 
-const Data = require('../shareData.json');
+
+
+
+
+
+const sData = require('../shareData.json');
 const ListViewDemo = React.createClass({
     getInitialState(){
         // 设置一些初始值 可以变化的
         // 什么状态下创建一个数据源
-        let ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!=r2});
+        let ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2});
         return{
-            dataSource:ds.cloneWithRows(Data.data);
+            dataSource:ds.cloneWithRows(sData.data)
         }
     },
     getDefaultProps(){
       // 设置默认值 不可变化的
 
     },
+    renderRow(rowData){ //把当前的数据传入
+        return(
+            <TouchableOpacity activeOpacity={.5}>
+                <View style={styles.innerStyle}>
+                    <Image source={{uri:rowData.icon}} style={styles.iconStyle}/>
+                    <Text>{rowData.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    },
     render(){
-        return(<View></View>)
+        return(<ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+            contentContainerStyle={styles.ListStyle}
+        />)
     }
 });
+const cols = 3;
+const {width} = require('Dimensions').get('window');
+const cellWh = 100;
+const vMargin = (width - cellWh*cols)/cols;
+const hMargin = 25;
 
 const styles = StyleSheet.create({
-    cellViewStyle:{
-        backgroundColor:'#e8e8e8'
+    iconStyle:{
+        width:80,
+        height:80
     },
-    leftImageStyle:{
-        width:60,
-        height:60
+    ListStyle:{
+        flexDirection:'row',
+        flexWrap:'wrap'
     },
-    topTitleStyle:{
-
-    },
-    bottomTitleStyle:{
-
+    innerStyle:{
+        width:cellWh,
+        height:cellWh,
+        marginLeft:vMargin,
+        marginTop:hMargin
     }
 });
 module.exports = ListViewDemo;
