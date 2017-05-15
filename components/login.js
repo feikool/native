5dfb52b0c6f98da4a411faaf9fde49b746324b71
feikool <feikool@sina.com>
2017-05-15 20:29:21 +0800

    init

--- components/login.js
+++ components/login.js
@@ -8,9 +8,23 @@ import {
     TouchableOpacity,  //点击效果
     Alert
 } from 'react-native';
+
+const Data = require('../shareData.json');
 const ListViewDemo = React.createClass({
+    getInitialState(){
+        // 设置一些初始值 可以变化的
+        // 什么状态下创建一个数据源
+        let ds = new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!=r2});
+        return{
+            dataSource:ds.cloneWithRows(Data.data);
+        }
+    },
+    getDefaultProps(){
+      // 设置默认值 不可变化的
+
+    },
     render(){
-        return()
+        return(<View></View>)
     }
 });
 
